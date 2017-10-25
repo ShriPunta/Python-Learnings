4022a9aa9fa645f8488b1719c5db6caf0ba322ea
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-10-24 17:26:42 -0700

    Classes! Signed-off-by: Shridhar Puntambekar <shridhar.puntambekar@gmail.com>

---
+++ Classes.py
@@ -0,0 +1,46 @@
+class Enemy:
+    health_points = 5
+    _foo__is_dead = False
+
+    def set_hp(self,points_to_set):
+        if self._foo__is_dead == False:
+            self.health_points = points_to_set
+        else:
+            print("Enemy is already dead")
+
+    def attack(self):
+        print("Hint confirmed")
+        self.health_points -= 1
+
+
+        print(self._foo__is_dead)
+        print(self.health_points)
+
+        if (self.health_points <= -1):
+            if not self._foo__is_dead:
+                self._foo__is_dead = True
+                print("Enemy Terminated")
+            else:
+              print("Enemy is  dead")
+        else:
+            print("Enemy has {} health remaining ".format(self.health_points))
+
+    def reveal_hp(self):
+        if self._foo__is_dead == False:
+            print(self.health_points, " health points remaining")
+        else:
+            print("Enemy is already dead")
+
+
+first_enemy = Enemy()
+
+try:
+    one_hp = int(input("Enter hit points for first enemy"))
+except ValueError or one_hp > 100:
+    print("Enter only numerical values and less than 100")
+    exit(0)
+
+#value_to_set = 10
+first_enemy.set_hp(one_hp)
+first_enemy.attack()
+first_enemy.reveal_hp()
