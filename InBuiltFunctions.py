351c46918fbeebe453933f6e3bb67485c30f43e1
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-10-28 16:50:24 -0700

    More InBuilt Funcs

--- InBuiltFunctions.py
+++ InBuiltFunctions.py
@@ -1,4 +1,10 @@
-#Functions such as Zip, Map, Filter, Reduce
+#Functions such as Lambda, Zip, Map, Filter, Reduce
+#'**********************************************************************************************************
+#The Lambda Function
+
+t2 = lambda x: x*6
+print(t2(2))
+
 #'**********************************************************************************************************
 #Zip Function
 A  = ['1','1','1','1','1','1']
@@ -35,3 +41,19 @@ print('3',less_than_3)
 
 from functools import reduce
 product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
+
+#**********************************************************************************************************
+#Reduce
+#Min Max Sorting of a dictionary
+
+just_a_dict = {
+    'A':1,
+    'B':5,
+    'C':3,
+    'D':9,
+    'E':10
+}
+#If you want to sort by the numbers, send it as the first parameters
+print(max(zip(just_a_dict.keys(),just_a_dict.values())))
+#print(sorted(just_a_dict.keys(),just_a_dict.values()))
+
