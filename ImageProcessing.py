46ba871cbc0741762cafa65d4474ba74aa156414
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-10-29 17:14:06 -0700

    Starting Network basics

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,7 +1,16 @@
 from PIL import Image
 
 img = Image.open((r'C:\Users\shrip\Pictures\url_downloads\85.jpeg'))
-print(img.size.)
+print(img.size)
+
+#PNG,JPEG,etc
 print(img.format)
 
-img.show()
+#if its RGB, RGBA
+print(img.mode)
+
+r ,g ,b, a = img.split()
+
+img2 = Image.merge("RGB", (g ,b ,a))
+#img.show()
+img2.show()
---
+++ NetworkBasics.py
@@ -0,0 +1,5 @@
+#Learn struct = it turns number/letters/strings and converts it to bytes format
+
+from struct import *
+
+packed_data =
