8f5a4d2e56f45e93060cfd08fa1f75d485371cb3
Shridhar Puntambekar <shridhar.puntambekar@gmail.com>
2017-11-06 22:30:13 -0800

    Testing

--- Python Basics/FilePhunc.py
+++ Python Basics/FilePhunc.py
@@ -9,3 +9,13 @@ fileRead = open(fileName, 'r')
 contents = fileRead.read()
 print(contents)
 fileRead.close()
+
+
+n = 3
+nfirstlines = []
+
+with open("bigFile.txt") as f, open("bigfiletmp.txt", "w") as out:
+    for x in xrange(n):
+        nfirstlines.append(next(f))
+    for line in f:
+        out.write(line)
--- Python Basics/test.py
+++ Python Basics/test.py
@@ -28,3 +28,44 @@ for i in range(1,11):
     L = K % 11
     #For printing in binary
     print(i,' ',L,' ',"{0:04b}".format(L))
+
+base_dir = r'C:\Users\shrip\Pictures\url_downloads\\'
+
+curr_filepath = base_dir+r"test\\"
+
+import os
+for root, dirs, files in os.walk(curr_filepath, topdown=True):
+      for filename in root:
+          print(root)
+          print('-----------------')
+print('******************')
+
+
+def defin_header():
+    return (r"Latitude,Longitude,dele_colum,Altitude,dele_colum,Date,Time \n")
+
+def line_pre_adder(filename):
+    f = fileinput.input(filename, inplace=1)
+    for xline in f:
+        if f.isfirstline():
+            print(defin_header().rstrip('\r\n') + '\n' + xline)
+        else:
+            print(xline)
+
+#delete first 6 lines
+def del_start_lines(filename):
+    f = open(filename)
+    lines = f.readlines()
+    f.seek(0,0)
+
+    f = open(filename, 'w')
+    f.write(defin_header())
+    f.write('\n')
+    f.writelines(lines[6:])
+    f.close()
+
+
+
+filename = r"C:\Users\shrip\Pictures\url_downloads\test\20081027115449.txt"
+del_start_lines(filename)
+#line_prepender(filename)
